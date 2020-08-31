c6f831e64aad4642ead04c2994de8e7c8dce1d78
Sameer <kumar.sam1612@gmail.com>
2020-08-31 13:34:48 +0530

    initial commit

---
+++ precache-manifest.291188cc45ecc984d8e62582ce593bf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "673be9f3dee0beccf2a8e714d3a0e556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32615602af0d9dc007c0",
+    "url": "/static/css/main.b8c9f0ba.chunk.css"
+  },
+  {
+    "revision": "dc64e97a4a659091a1fb",
+    "url": "/static/js/2.ac2ab43c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.ac2ab43c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32615602af0d9dc007c0",
+    "url": "/static/js/main.c3e51d6b.chunk.js"
+  },
+  {
+    "revision": "589f5ddec23114e2fc51",
+    "url": "/static/js/runtime-main.26286738.js"
+  }
+]);
